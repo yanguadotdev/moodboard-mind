054cdd96d302f264f005ae13bd6b18a0bd3dd0dc
yanguadotdev <yanguasamir05@gmail.com>
2026-04-13 10:18:26 -0500

    fix: resolve hydratation mismatch by removing nested html and body tags

--- app/(protected)/dashboard/layout.tsx
+++ app/(protected)/dashboard/layout.tsx
@@ -11,11 +11,5 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body>
-        <MindStoreProvider>{children}</MindStoreProvider>
-      </body>
-    </html>
-  );
+  return <MindStoreProvider>{children}</MindStoreProvider>;
 }

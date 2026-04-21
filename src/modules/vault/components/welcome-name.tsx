6e4c5bbf5155f86499e6e5b078da14a68f90999c
yanguadotdev <yanguasamir05@gmail.com>
2026-04-21 09:37:15 -0500

    refactor(auth): move profile queries to auth module services

--- src/modules/vault/components/welcome-name.tsx
+++ src/modules/vault/components/welcome-name.tsx
@@ -1,11 +1,11 @@
-import { getUserProfile } from '@spb/queries';
+import { getUserProfile } from '@auth/services/queries'
 
 export async function WelcomeName() {
-  const firstName = await getUserProfile();
+  const firstName = await getUserProfile()
 
-  return <span className='text-foreground ml-2'>{firstName}</span>;
+  return <span className='text-foreground ml-2'>{firstName}</span>
 }
 
 export function WelcomeFallback() {
-  return <span className='ml-2 text-base w-28 h-6 bg-zinc-800 animate-pulse rounded-md inline-block align-middle' />;
+  return <span className='ml-2 text-base w-28 h-6 bg-zinc-800 animate-pulse rounded-md inline-block align-middle' />
 }

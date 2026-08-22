f77a895143b11a1d95cc51ed7752761d849841f0
Gael Alejandro Segura Mendoza <gaelsegura.mendoza@gmail.com>
2026-08-21 22:38:22 -0600

    [UI] getting rid of overflow content and updating background color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata, Viewport } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata, Viewport } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
   subsets: ['latin']
-});
+})
 
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin']
-});
+})
 
 export const viewport: Viewport = {
   themeColor: '#000000',
   width: 'device-width',
   initialScale: 1
-};
+}
 
 export const metadata: Metadata = {
   title: {
@@ -26,16 +26,18 @@ export const metadata: Metadata = {
   description: 'Organiza tus referencias visuales e ideas de forma minimalista y rápida.',
   keywords: ['SaaS', 'Design', 'Moodboard', 'Productivity'],
   authors: [{ name: 'Samir Yangua' }, { name: 'Gael Segura' }]
-};
+}
 
 export default function RootLayout({
   children
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <html lang='en'>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark`}>{children}</body>
+    <html lang='es'>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#f0f2f5] overflow-hidden`}>
+        {children}
+      </body>
     </html>
-  );
+  )
 }
